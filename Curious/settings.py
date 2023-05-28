45a9dcb36f42060e82f777b662955fdcef46f965
Yair <yairmasterlol@gmail.com>
2023-05-27 19:44:00 -0600

    2023-05-27-07:44:00

--- Curious/settings.py
+++ Curious/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-&i$&@#=%i5qu5pg0k=j5c#(&1=i&f#d&csr%c&8sea_(e0cw^d"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 CSRF_TRUSTED_ORIGINS = [
